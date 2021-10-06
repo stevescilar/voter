9999ec697f5dfa428fbeb2464e09b059727199eb
stevescilar <stevemuambi@gmail.com>
2021-10-06 15:42:07 +0300

    evening tackle

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import context, loader 
+
+from . models import Question
 
 def index(request):
-    return HttpResponse("voter is here")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context= {'latest_question_list':latest_question_list}
+    return render(request,'polls/index.html',context)
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id )
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s." % question_id)
